5a393b397fe23f654ab6ebaa344b1cd8eb73f1a6
김윤기 <iskull@gim-yungiui-MacBookPro.local>
2020-12-22 16:47:50 +0900

    login without session

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const path = require('path');
+const User = require('../models/user');
 
 const router = express.Router();
 
 router.use(express.urlencoded({extended: true}));
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
     try{
-        res.render('login', {'name': req.body.id});
+        const user = await User.findOne({
+            where: {name: req.body.id}
+        });
+        if(user){
+            res.render('login', {'name': req.body.id, has_user: true});
+        }
+        else{
+            res.render('login', {has_user: false});
+        }
     }
     catch(err){
         console.log(err);
